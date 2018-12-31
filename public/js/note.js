b2b3ea854968f4f45be57deb2b9dcc7bbf200936
unknown <clfrth89@gmail.com>
2018-12-31 10:50:03 +0900

    첫번째커밋

---
+++ public/js/note.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCuO5OPdr5VZvlAHgmCZmOExBEdI1-D3nc",
+    authDomain: "cgoon-note.firebaseapp.com",
+    databaseURL: "https://cgoon-note.firebaseio.com",
+    projectId: "cgoon-note",
+    storageBucket: "cgoon-note.appspot.com",
+    messagingSenderId: "820211052518"
+  };
+  firebase.initializeApp(config);
